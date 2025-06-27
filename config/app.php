546a05e5875151b452a388bac49b7914786b85a4
MarcondesJuniorDev <mncjdev@gmail.com>
2025-06-27 10:55:19 -0400

    Tarefa: atualizar a configuração do aplicativo para a localização brasileira
    - Fuso horário alterado para 'América/Manaus'
    - Definir localidade padrão para 'pt_BR'
    
    Tarefa: adicionar suporte ao idioma português
    
    - Traduções adicionadas para várias strings do aplicativo em pt_BR.json
    - Actions.php criado para frases de ação comuns em português
    - Auth.php adicionado para mensagens de autenticação em português
    - http-statuses.php introduzido para mensagens de status HTTP em português
    - pagination.php criado para mensagens de paginação em português
    - Passwords.php adicionado para mensagens relacionadas a senhas em português
    - Validation.php implementado para mensagens de validação em português

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Manaus',
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', default: 'pt_BR'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
